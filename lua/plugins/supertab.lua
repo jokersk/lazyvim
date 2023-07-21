0fb196fe05ff87f2c5cdf861531e2cd8da08bd7c
Szeto <jokersk119@gmail.com>
2023-07-21 09:55:15 +0800

    wip

--- lua/plugins/supertab.lua
+++ lua/plugins/supertab.lua
@@ -1,4 +1,6 @@
 return {
+  -- Use <tab> for completion and snippets (supertab)
+  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
     "L3MON4D3/LuaSnip",
     keys = function()
