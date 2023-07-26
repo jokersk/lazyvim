c28c03d2b24e97b093a723836d00035a1e97cac5
Szeto <jokersk119@gmail.com>
2023-07-26 17:05:36 +0800

    Update neotest.lua

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -3,5 +3,15 @@ return {
   {
     "nvim-neotest/neotest",
     opts = { adapters = { "neotest-pest" } },
+    keys = {
+      {
+        "<leader>tl",
+        function()
+          require("neotest").run.run_last()
+          require("neotest").output_panel.open()
+        end,
+        desc = "Run last test",
+      },
+    },
   },
 }
