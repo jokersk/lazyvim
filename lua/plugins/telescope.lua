477bbc767ca7682b808ae0b96a297f536cd85bd9
Szeto <jokersk119@gmail.com>
2023-07-28 14:11:12 +0800

    wip

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,16 @@
 return {
   "nvim-telescope/telescope.nvim",
+  keys = {
+    {
+      "<leader>fF",
+      "[[<cmd>lua require('telescope.builtin').find_files({ no_ignore = true, prompt_title = 'All Files' })<CR>]]",
+      desc = "Search All files",
+    },
+  },
   opts = {
     defaults = {
       file_ignore_patterns = {
-        "vendor",
+        "node_modules",
       },
     },
   },
